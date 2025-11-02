409b4bfb53ad3c0c03488ab04509409eafcc0c30
SeeTwoo <walti.bee@gmail.com>
2025-11-02 22:44:28 +0100

    started refactoring the expansion logic. there is an invalid read when trying to expand a non existing variable

--- src/expansion.c
+++ src/expansion.c
@@ -6,8 +6,6 @@
 #include "key_value.h"
 #include "messages.h"
 
-char	*get_kv_n_value(t_key_value *env, char *key, size_t len);
-
 size_t	ft_charcspn(char const *s, char const c) {
 	size_t	i = 0;
 
@@ -25,31 +23,31 @@ static size_t	var_name_len(char const *s) {
 		i++;
 	return (i);
 }
-///THERE MUST BE A PRETTIER WAY THAN THIS
+
 static char	*replace_key_with_value(char *s, char *value, char *dollar, size_t key_len) {
-	char	*empty = "";
-	if (!value)
-		value = empty;
-	char	*dest = malloc(sizeof(char) * (strlen(s) + 1 - key_len + strlen(value)));
-	char	*end_s = &s[dollar + 1 - s + key_len];
+	size_t	dest_len = strlen(s) + 1 - key_len + strlen(value);
+	char	*dest = malloc(sizeof(char) * dest_len);
+	char	*end_s = dollar + 1 + key_len;
 
 	if (!dest) 
 		return (dprintf(2, "%s%s: %s\n", WARN_HD, ERR_MALLOC, NO_EXPAND), s);
-	memcpy(dest, s, dollar - s);
-	memcpy(&dest[dollar - s], value, strlen(value));
-	memcpy(&dest[dollar - s + strlen(value)], end_s, strlen(end_s) + 1);
+	*dollar = '\0';
+	sprintf(dest, "%s%s%s", s, value, end_s);
 	free(s);
 	return (dest);
 }
 
 static char	*replace(char *s, char *dollar, t_key_value *env) {
 	char	*value;
+	char	empty[] = "";
 	size_t	key_len;
 
 	if (!isalpha(dollar[1]) && dollar[1] != '_')
 		return (s);
-	key_len = var_name_len(&dollar[1]);
-	value = get_kv_n_value(env, &dollar[1], key_len);
+	key_len = var_name_len(dollar + 1);
+	value = get_kv_n_value(env, dollar + 1, key_len);
+	if (!value)
+		value = empty;
 	return (replace_key_with_value(s, value, dollar, key_len));
 }
 
@@ -63,7 +61,7 @@ static char	*expand_string(char *s, t_key_value *env) {
 			i++;
 			i += ft_charcspn(&s[i], '\'');
 		} else if (s[i] == '$') {
-			s = replace(s, &s[i], env);
+			s = replace(s, s + i, env);
 			i++;
 		} else {
 			i++;
